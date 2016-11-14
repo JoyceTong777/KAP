27d41edf151d6d09ffb8ceda1223d10c384c5e8c
xiao li <sean@kresge-38-158.resnet.ucsc.edu>
2016-11-14 03:22:16 -0800

    add setting component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { IndexdbService } from './shared/indexdb.service';
+import { AppModule } from './app.module';
+import { ResourceService } from './resource/resource.service';
+import { Resource } from './resource/resource';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+  selector: 'tg-app',
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{ 
+  resources: Resource[];
+
+  constructor(private resourceService: ResourceService, private indexdbService: IndexdbService) { }
+
+  getResources(): void {
+    this.resourceService.getResources().then(resources => this.resources = resources);
+  }
+
+  ngOnInit(): void {
+    this.indexdbService.createApiserverSchema();
+    this.indexdbService.openDatabase();
+  }
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,54 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { AboutComponent } from './about/about.component';
+import { QueryComponent } from './query/query.component';
+import { ClusterComponent } from './cluster/cluster.component';
+import { SettingComponent } from './setting/setting.component';
+import { ResourceService } from './resource/resource.service';
+import { IndexdbService } from './shared/indexdb.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/about',
+        pathMatch: 'full'
+      },
+      {
+        path: 'setting',
+        component: SettingComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'cluster',
+        component: ClusterComponent
+      },
+      {
+        path: 'query',
+        component: QueryComponent
+      }
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    SettingComponent,
+    AboutComponent,
+    ClusterComponent,
+    QueryComponent 
+  ],
+  providers: [ 
+    ResourceService, IndexdbService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
