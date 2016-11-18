4cc32ad8247db3297c02319bb00fc1ca45573186
xiao li <sean@kresge-39-140.resnet.ucsc.edu>
2016-11-18 14:11:04 -0800

    add carousel

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tg-about',
   moduleId: module.id,
-  templateUrl: 'about.component.html'
+  templateUrl: 'about.component.html',
+  styleUrls: ['about.component.css']
 })
 
 export class AboutComponent { }
